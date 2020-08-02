77ec12a59d9a56499112988e2f10027009325dd2
Mark Stega <mark2002@stega.us>
2020-08-02 10:41:21 -0400

    Initial content commit

---
+++ BlazorMdc-Project-Guidelines/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlazorMdc_Project_Guidelines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello BlazorMdc-Project-Guidelines!");
+        }
+    }
+}
